b3d2661ca1b5570f31ee30ee1feb8e2b5d8cbb75
leshengping <lanou@YF-10.local>
2017-01-11 22:32:04 +0800

    增加显示速度和加速度试例
    增加显示速度和加速度试例

--- Example/SPHTTPSessionManager/DownloadViewController.m
+++ Example/SPHTTPSessionManager/DownloadViewController.m
@@ -18,13 +18,25 @@
 @property (weak, nonatomic) IBOutlet UIButton *downloadButton;
 @property (weak, nonatomic) IBOutlet UIProgressView *progressView;
 @property (weak, nonatomic) IBOutlet UILabel *promptLabel;
+@property (weak, nonatomic) IBOutlet UILabel *speedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *accelerationLabel;
 
+@property (nonatomic, strong) NSTimer *timer;
+
+// 保存已经下载完成的数据大小
+@property (nonatomic, assign) CGFloat completedLength;
+// 保存1秒前下载完成的数据大小
+@property (nonatomic, assign) CGFloat lastCompletedLength;
+// 保存1秒前的速度值
+@property (nonatomic, assign) CGFloat lastSpeed;
 @end
 
 @implementation DownloadViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.progressView.transform = CGAffineTransformMakeScale(1.0, 5.0);
     
     self.progressView.progress = NetworkManager.storedDownloadProgress;
     
@@ -55,6 +67,22 @@
     }
     
     sender.selected = !sender.selected;
+    
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(showSpeedAndAcceleration) userInfo:nil repeats:YES];
+    
+}
+
+// 显示速度和加速度
+- (void)showSpeedAndAcceleration {
+    // 速度等于1秒间隔内两数据之差
+    CGFloat speed = (self.completedLength - self.lastCompletedLength)/1000;
+    self.speedLabel.text = [NSString stringWithFormat:@"%.0fK/S",speed]; // 除以1000是为了精确到百位
+    // 加速度等于1秒间隔内两速度之差(不考虑正负，故取绝对值)
+    CGFloat acceleration = fabs(speed - self.lastSpeed);
+    self.accelerationLabel.text = [NSString stringWithFormat:@"+%.0f",acceleration];
+    self.lastCompletedLength = self.completedLength;
+    self.lastSpeed = speed;
 }
 
 /** 删除 */
@@ -83,6 +111,8 @@
             self.progressView.progress = 1.0 * progress.completedUnitCount / progress.totalUnitCount;
             self.promptLabel.text = [NSString stringWithFormat:@"%.1fM/%.1fM",progress.completedUnitCount / (1000.0 * 1000.0),progress.totalUnitCount / (1000.0*1000.0)];
         });
+         
+         self.completedLength = progress.completedUnitCount;
         
     } complete:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
         // 如若要更新UI,需要回到主线程
@@ -96,11 +126,18 @@
             dispatch_async(dispatch_get_main_queue(), ^(void){
                 [self.downloadButton setTitle:@"下载完成" forState:UIControlStateNormal];
                 self.downloadButton.enabled = NO;
+                
+                [self.timer invalidate];
+                self.timer = nil;
+                self.promptLabel.text = @"";
+                self.accelerationLabel.text = @"";
+                self.speedLabel.text = @"";
             });
             NSLog(@"下载完成");
         } else {
             //NSLog(@"error:%@",error);
             //NSLog(@"下载失败");
+
             if ([error.domain isEqualToString:NSURLErrorDomain] && error.code == -1005 ) {
                 NSLog(@"网络断开了");
                
